6ad302108baccdafdace5462faabb1af74d0d212
Samidh Patel <spatel13456@gmail.com>
2019-01-08 18:57:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,8 +5,17 @@ def countdown(number)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
-  sleep 5
   end 
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_to_sleep(number)
+  number = 10
+  while number > 10
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep 5
+  end
   return "HAPPY NEW YEAR!"
 end
 
